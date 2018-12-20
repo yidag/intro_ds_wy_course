f0f9072f096ea16e80941f98ec85a51e725c7046
Gen TANG <gen.tang86@gmail.com>
2018-12-20 23:46:57 +0800

    linear overfitting

---
+++ ch04_linear/code/linear_overfitting.py
@@ -0,0 +1,150 @@
+# -*- coding: UTF-8 -*-
+"""
+此脚本用于展示过度拟合的问题
+"""
+
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+
+
+def read_data(path):
+    """
+    使用pandas读取数据
+    
+    参数
+    ----
+    path: String，数据的路径
+    
+    返回
+    ----
+    data: DataFrame，建模数据
+    """
+    data = pd.read_csv(path)
+    return data
+
+
+def evaluate_model(model, test_data, features, labels, featurizer):
+    """
+    计算线性模型的均方差和决定系数
+    
+    参数
+    ----
+    model : LinearRegression, 训练完成的线性模型
+    
+    testData : DataFrame，测试数据
+    
+    features : list[str]，特征名列表
+    
+    labels : list[str]，标签名列表
+    
+    返回
+    ----
+    error : np.float64，均方差
+    
+    score : np.float64，决定系数
+    """
+    # 均方差(The mean squared error)，均方差越小越好
+    error = np.mean(
+        (model.predict(featurizer.fit_transform(test_data[features])) - test_data[labels]) ** 2)
+    # 决定系数(Coefficient of determination)，决定系数越接近1越好
+    score = model.score(featurizer.fit_transform(test_data[features]), test_data[labels])
+    return error, score
+
+
+def train_model(train_data, features, labels, featurizer):
+    """
+    利用训练数据，估计模型参数
+    
+    参数
+    ----
+    trainData : DataFrame，训练数据集，包含特征和标签
+    
+    features : 特征名列表
+    
+    labels : 标签名列表
+    
+    返回
+    ----
+    model : LinearRegression, 训练好的线性模型
+    """
+    # 创建一个线性回归模型
+    model = linear_model.LinearRegression(fit_intercept=False)
+    # 训练模型，估计模型参数
+    model.fit(featurizer.fit_transform(train_data[features]), train_data[labels])
+    return model
+
+
+def visualize_model(model, featurizer, data, features, labels, evaluation):
+    """
+    模型可视化
+    """
+    # 为在Matplotlib中显示中文，设置特殊字体
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    # 创建一个图形框
+    fig = plt.figure(figsize=(10, 10), dpi=80)
+    # 在图形框里只画一幅图
+    for i in range(4):
+        ax = fig.add_subplot(2, 2, i+1)
+        _visualization(ax, data, model[i], featurizer[i], evaluation[i], features, labels)
+    plt.show()
+
+
+def _visualization(ax, data, model, featurizer, evaluation, features, labels):
+    """
+    实现可视化
+    """
+    # 画点图，用蓝色圆点表示原始数据
+    ax.scatter(data[features], data[labels], color='b')
+    # 画线图，用红色线条表示模型结果
+    ax.plot(data[features], model.predict(featurizer.fit_transform(data[features])),
+            color="r")
+    # 显示均方差和决定系数
+    ax.text(0.01, 0.99,
+            u'%s%.3f\n%s%.3f' % ("mse: ", evaluation[0], "R2: ", evaluation[1]),
+            style="italic", verticalalignment="top", horizontalalignment="left",
+            transform=ax.transAxes, color="m", fontsize=13)
+
+
+def run_model(data):
+    """
+    运行模型
+    """
+    features = ["x"]
+    labels = ["y"]
+    # 划分训练集和测试集
+    train_data = data[:15]
+    test_data = data[15:]
+    featurizer = []
+    overfitting_model = []
+    overfitting_evaluation = []
+    model = []
+    evaluation = []
+    for i in range(1, 11, 3):
+        featurizer.append(PolynomialFeatures(degree=i))
+        # 产生并训练模型
+        overfitting_model.append(train_model(train_data, features, labels, featurizer[-1]))
+        model.append(train_model(data, features, labels, featurizer[-1]))
+        # 评价模型效果
+        overfitting_evaluation.append(
+            evaluate_model(overfitting_model[-1], test_data, features, labels, featurizer[-1]))
+        evaluation.append(evaluate_model(model[-1], data, features, labels, featurizer[-1]))
+    # 图形化模型结果
+    visualize_model(model, featurizer, data, features, labels, evaluation)
+    visualize_model(overfitting_model, featurizer,
+                    data, features, labels, overfitting_evaluation)
+
+
+if __name__ == "__main__":
+    home_path = os.path.dirname(os.path.abspath(__file__))
+    # Windows下的存储路径与Linux并不相同
+    if os.name == "nt":
+        data_path = "%s\\simple_example.csv" % home_path
+    else:
+        data_path = "%s/simple_example.csv" % home_path
+    data = read_data(data_path)
+    run_model(data)
---
+++ ch04_linear/code/linear_reg.py
@@ -0,0 +1,96 @@
+# -*- coding: UTF-8 -*-
+"""
+此脚本用于展示使用惩罚项解决模型幻觉的问题
+"""
+
+
+import os
+
+import numpy as np
+import statsmodels.api as sm
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def read_data(path):
+    """
+    使用pandas读取数据
+    
+    参数
+    ----
+    path: String，数据的路径
+    
+    返回
+    ----
+    data: DataFrame，建模数据
+    """
+    data = pd.read_csv(path)
+    return data
+
+
+def generate_random_var():
+    """
+    """
+    np.random.seed(4873)
+    return np.random.randint(2, size=20)
+
+
+def train_model(x, y, alpha):
+    """
+    """
+    model = linear_model.Lasso(alpha=alpha, fit_intercept=False)
+    model.fit(x, y)
+    return model
+
+def visualize_model(X, Y, alphas, coefs):
+    """
+    模型可视化
+    """
+    # 创建一个图形框
+    fig = plt.figure(figsize=(6, 6), dpi=80)
+    # 在图形框里只画一幅图
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(alphas, coefs[:, 1], "r:", label=u'%s' % "a")
+    ax.plot(alphas, coefs[:, 2], "g", label=u'%s' % "b")
+    ax.plot(alphas, coefs[:, 0], "b-.", label=u'%s' % "c")
+    legend = plt.legend(loc=4, shadow=True)
+    legend.get_frame().set_facecolor("#6F93AE")
+    ax.set_yticks(np.arange(-1, 1.3, 0.3))
+    ax.set_xscale("log")
+    ax.set_xlabel("$alpha$")
+    plt.show()
+
+
+def run_model(data):
+    """
+    运行模型
+    """
+    features = ["x"]
+    labels = ["y"]
+    Y = data[labels]
+    X = data[features]
+    # 加入新的随机变量，次变量的系数应为0
+    X["z"] = generate_random_var()
+    # 加入常变量const
+    X = sm.add_constant(X)
+    alphas = np.logspace(-4, -0.5, 100)
+    coefs = []
+    for alpha in alphas:
+        model = train_model(X, Y, alpha)
+        coefs.append(model.coef_)
+    coefs = np.array(coefs)
+    print(coefs)
+    # 可视化惩罚项效果
+    visualize_model(X, Y, alphas, coefs)
+
+
+if __name__ == "__main__":
+    home_path = os.path.dirname(os.path.abspath(__file__))
+    # Windows下的存储路径与Linux并不相同
+    if os.name == "nt":
+        data_path = "%s\\simple_example.csv" % home_path
+    else:
+        data_path = "%s/simple_example.csv" % home_path
+    data = read_data(data_path)
+    run_model(data)
